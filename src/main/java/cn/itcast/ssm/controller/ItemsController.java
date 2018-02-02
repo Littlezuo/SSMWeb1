480689c1d47eed6d18e5f6323b5b82f5638c9619
Littlezuo <767770939@qq.com>
2018-02-02 13:39:08 +0800

    项目框架完成

--- src/main/java/cn/itcast/ssm/controller/ItemsController.java
+++ src/main/java/cn/itcast/ssm/controller/ItemsController.java
@@ -15,7 +15,6 @@ import cn.itcast.ssm.po.ItemsCustom;
 import cn.itcast.ssm.service.ItemsService;
 
 /**
- * 
  * <p>
  * Title: ItemsController
  * </p>
@@ -25,111 +24,109 @@ import cn.itcast.ssm.service.ItemsService;
  * <p>
  * Company: www.itcast.com
  * </p>
- * 
+ *
  * @author 传智.燕青
- * @date 2015-3-20下午3:04:57
  * @version 1.0
+ * @date 2015-3-20下午3:04:57
  */
 @Controller
 //定义url的根路径，访问时根路径+方法的url
 @RequestMapping("/items")
 public class ItemsController {
-	
-	//注入service
-	@Autowired
-	private ItemsService itemsService;
 
-	@RequestMapping("/queryItems")
-	public ModelAndView queryItems(HttpServletRequest request) throws Exception {
-		
-		System.out.println(request.getParameter("id"));
-	
-		//调用service查询商品列表
-		List<ItemsCustom> itemsList = itemsService.findItemsList(null);
+    //注入service
+    @Autowired
+    private ItemsService itemsService;
 
-		ModelAndView modelAndView = new ModelAndView();
-		modelAndView.addObject("itemsList", itemsList);
-		// 指定逻辑视图名
-		modelAndView.setViewName("itemsList");
+    @RequestMapping("/queryItems")
+    public ModelAndView queryItems(HttpServletRequest request) throws Exception {
 
-		return modelAndView;
-	}
-	
-	//商品修改页面显示
-	//使用method=RequestMethod.GET限制使用get方法
-//	@RequestMapping(value="/editItems",method={RequestMethod.GET})
-//	public ModelAndView editItems()throws Exception{
-//		
-//		ModelAndView modelAndView = new ModelAndView();
-//		
-//		//调用 service查询商品信息
-//		ItemsCustom itemsCustom = itemsService.findItemsById(1);
-//		//将模型数据传到jsp
-//		modelAndView.addObject("item", itemsCustom);
-//		//指定逻辑视图名
-//		modelAndView.setViewName("editItem");
-//		
-//		return modelAndView;
-//		
-//	}
-	
-	//方法返回 字符串，字符串就是逻辑视图名，Model作用是将数据填充到request域，在页面展示
-	@RequestMapping(value="/editItems",method={RequestMethod.GET})
-	public String editItems(Model model,Integer id)throws Exception{
-		
-		//调用 service查询商品信息
-		ItemsCustom itemsCustom = itemsService.findItemsById(id);
-		
-		model.addAttribute("item", itemsCustom);
-		
-		
-		//return "editItem_2";
-		return "editItem";
-		
-	}
-	
-	//方法返回void
-//	@RequestMapping(value="/editItems",method={RequestMethod.GET})
-//	public void editItems(
-//			HttpServletRequest request,
-//			HttpServletResponse response,
-//			//@RequestParam(value = "item_id", required = false, defaultValue = "1") Integer id
-//			Integer id
-//			)
-//			throws Exception {
-//	
-//		//调用 service查询商品信息
-//		ItemsCustom itemsCustom = itemsService.findItemsById(id);
-//		request.setAttribute("item", itemsCustom);
-//		//注意如果使用request转向页面，这里指定页面的完整路径
-//		request.getRequestDispatcher("/WEB-INF/jsp/editItem.jsp").forward(request, response);
-//		
-//	}
-	
-	//商品修改提交
-	
-	//itemsQueryVo是包装类型的pojo
-	@RequestMapping("/editItemSubmit")
-//	public String editItemSubmit(Integer id,ItemsCustom itemsCustom,
-//			ItemsQueryVo itemsQueryVo)throws Exception{
-	public String editItemSubmit(Integer id,ItemsCustom itemsCustom)throws Exception{
-		
-		//调用service接口更新商品信息
-		itemsService.updateItems(id, itemsCustom);
-		//请求重定向
-		return "redirect:queryItems.action";
-		//转发
-//		return "forward:queryItems.action";
-	}
-	
-	//自定义属性编辑器
-//	@InitBinder
-//	public void initBinder(WebDataBinder binder) throws Exception {
-//		// Date.class必须是与controler方法形参pojo属性一致的date类型，这里是java.util.Date
-//		binder.registerCustomEditor(Date.class, new CustomDateEditor(
-//				new SimpleDateFormat("yyyy-MM-dd HH-mm-ss"), true));
-//	}
+        System.out.println(request.getParameter("id"));
 
-	
+        //调用service查询商品列表
+        List<ItemsCustom> itemsList = itemsService.findItemsList(null);
+
+        ModelAndView modelAndView = new ModelAndView();
+        modelAndView.addObject("itemsList", itemsList);
+        // 指定逻辑视图名
+        modelAndView.setViewName("itemsList");
+
+        return modelAndView;
+    }
+
+    //商品修改页面显示
+    //使用method=RequestMethod.GET限制使用get方法
+    //	@RequestMapping(value="/editItems",method={RequestMethod.GET})
+    //	public ModelAndView editItems()throws Exception{
+    //
+    //		ModelAndView modelAndView = new ModelAndView();
+    //
+    //		//调用 service查询商品信息
+    //		ItemsCustom itemsCustom = itemsService.findItemsById(1);
+    //		//将模型数据传到jsp
+    //		modelAndView.addObject("item", itemsCustom);
+    //		//指定逻辑视图名
+    //		modelAndView.setViewName("editItem");
+    //
+    //		return modelAndView;
+    //
+    //	}
+
+    //方法返回 字符串，字符串就是逻辑视图名，Model作用是将数据填充到request域，在页面展示
+    @RequestMapping(value = "/editItems", method = {RequestMethod.GET})
+    public String editItems(Model model, Integer id) throws Exception {
+
+        //调用 service查询商品信息
+        ItemsCustom itemsCustom = itemsService.findItemsById(id);
+
+        model.addAttribute("item", itemsCustom);
+
+        //return "editItem_2";
+        return "editItem";
+
+    }
+
+    //方法返回void
+    //	@RequestMapping(value="/editItems",method={RequestMethod.GET})
+    //	public void editItems(
+    //			HttpServletRequest request,
+    //			HttpServletResponse response,
+    //			//@RequestParam(value = "item_id", required = false, defaultValue = "1") Integer id
+    //			Integer id
+    //			)
+    //			throws Exception {
+    //
+    //		//调用 service查询商品信息
+    //		ItemsCustom itemsCustom = itemsService.findItemsById(id);
+    //		request.setAttribute("item", itemsCustom);
+    //		//注意如果使用request转向页面，这里指定页面的完整路径
+    //		request.getRequestDispatcher("/WEB-INF/jsp/editItem.jsp").forward(request, response);
+    //
+    //	}
+
+    //商品修改提交
+
+    //itemsQueryVo是包装类型的pojo
+    @RequestMapping("/editItemSubmit")
+    //	public String editItemSubmit(Integer id,ItemsCustom itemsCustom,
+    //			ItemsQueryVo itemsQueryVo)throws Exception{
+    public String editItemSubmit(Integer id, ItemsCustom itemsCustom) throws Exception {
+
+        //调用service接口更新商品信息
+        itemsService.updateItems(id, itemsCustom);
+        //请求重定向
+        return "redirect:queryItems.action";
+        //转发
+        //		return "forward:queryItems.action";
+    }
+
+    //自定义属性编辑器
+    //	@InitBinder
+    //	public void initBinder(WebDataBinder binder) throws Exception {
+    //		// Date.class必须是与controler方法形参pojo属性一致的date类型，这里是java.util.Date
+    //		binder.registerCustomEditor(Date.class, new CustomDateEditor(
+    //				new SimpleDateFormat("yyyy-MM-dd HH-mm-ss"), true));
+    //	}
+
 
 }
